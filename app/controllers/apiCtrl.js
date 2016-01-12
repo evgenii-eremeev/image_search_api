59446a8f5ce7fbb805d225e2d365a29289c54aa1
jaycrypto <jaycrypto@gmail.com>
2016-01-12 14:09:56 +0000

    api works at /api/imagesearch'

---
+++ app/controllers/apiCtrl.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var bingImageRequest = require(process.cwd() + '/app/utils/bingImageRequest.js');
+
+
+
+function Api() {
+    this.imageSearch = function(req, res) {
+        var keywords = req.params.keywords;
+        
+        bingImageRequest(keywords, 10, function (error, request, body) {
+            var results = JSON.parse(body).d.results;
+            var pics = results.map(function(pic) {
+                return {
+                    url: pic.MediaUrl,
+                    snippet: pic.Title,
+                    thumbnail: pic.Thumbnail.MediaUrl,
+                    context: pic.SourceUrl  
+                };
+            });
+            res.send(pics); 
+        });
+    };
+}
+
+module.exports = Api;
--- server.js
+++ server.js
@@ -2,16 +2,20 @@
 
 var express = require("express");
 var mongoose = require("mongoose");
+var ApiCtrl = require(process.cwd() + '/app/controllers/apiCtrl.js');
 require('dotenv').load();
 
 var app = express();
 
 //mongoose.connect('mongodb://localhost:27017/shorts');
 
+var apiCtrl = new ApiCtrl();
 
 app.get('/', function(req, res) {
     res.sendFile(process.cwd() + '/public/index.html');
 });
+
+app.get('/api/imagesearch/:keywords', apiCtrl.imageSearch);
 
 
 var port = process.env.PORT || 8080;
